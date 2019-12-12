f569ce0e23133467c2d14b1652f2b7a0b86f3894
aj3120 <aj3120@gmail.com>
2019-12-13 01:26:29 +0530

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,16 +1,19 @@
 def isListOfInts(list_numbers):
-    int_flag = True
-    for i in list_numbers :
-        if type(i) is not int:
-            int_flag = False
-            break
-    return int_flag
+    if type(list_numbers) is list:
+        int_flag = True
+        for i in list_numbers :
+            if type(i) is not int:
+                int_flag = False
+                break
+        return int_flag
+    else:
+        raise ValueError('{} arg not of <list> type'.format(list_numbers))
 
 def testList(list_numbers):
-    if type(list_numbers) is list:
-        print(isListOfInts(list_numbers))
-    else:
-        print('Value Error : {} arg not of <list> type'.format(list_numbers))
+    result = isListOfInts(list_numbers)
+    if(result==True or result == False):
+        print('{} --> {}'.format(list_numbers,result))
+        
 
 
-testList([])
+testList([1,5,7])
